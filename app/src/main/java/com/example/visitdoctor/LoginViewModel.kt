4adb546c039e912fa3eafab9c5b6e73fca851002
OneAkash <akashtazbir@hotmail.com>
2024-03-01 11:31:08 +0600

    A little bit file and directory upgrade

--- app/src/main/java/com/example/visitdoctor/LoginViewModel.kt
+++ app/src/main/java/com/example/visitdoctor/LoginViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.visitdoctor
+//
+//public override fun onStart() {
+//    super.onStart()
+//    // Check if user is signed in (non-null) and update UI accordingly.
+//    val currentUser = auth.currentUser
+//    if (currentUser != null) {
+//        reload()
+//    }
+//}
 
-public override fun onStart() {
-    super.onStart()
-    // Check if user is signed in (non-null) and update UI accordingly.
-    val currentUser = auth.currentUser
-    if (currentUser != null) {
-        reload()
-    }
-}
